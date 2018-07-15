fb10890a863b711516cf16c4734e6fe126ac7f82
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-15 11:40:59 -0600

    add tests and methods for if activity exists and if it has a name

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,16 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+  def test_it_exists
+    activity = Activity.new("Fred")
+    assert_instance_of Activity, activity
+  end
+
+  def test_it_has_a_name
+    activity = Activity.new("Fred")
+    assert_equal "Fred", activity.name
+  end
+  # def test_it_has_participants
+  #   activity = Activity.new
+  # end
 end
