530bcb6391f1c4a1527869abe6450b139943d27c
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-15 14:30:23 -0600

    add methods & tests for activity

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,7 +1,22 @@
 class Activity
-  attr_reader :name, :participants
+  attr_reader :name, :participants, :base_cost
   def initialize(name)
     @name = name
-    @participants = {}
+    @participants = []
+    @base_cost = 0
+  end
+
+  def add_particpants(participant)
+    @participants << participant
+  end
+
+  def base_cost(cost)
+    @base_cost = cost
+  end
+
+  def cost_per_person
+    # divide up base cost by number of people
+    number_of_people = participants.length
+    @base_cost / number_of_people
   end
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -2,18 +2,44 @@ require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/activity'
+require 'pry'
 
 class ActivityTest < Minitest::Test
+
+  def setup
+    @activity = Activity.new("paired assessment practice")
+  end
+
   def test_it_exists
-    activity = Activity.new("Fred")
-    assert_instance_of Activity, activity
+    assert_instance_of Activity, @activity
   end
 
   def test_it_has_a_name
-    activity = Activity.new("Fred")
-    assert_equal "Fred", activity.name
+    assert_equal "paired assessment practice", @activity.name
   end
-  # def test_it_has_participants
-  #   activity = Activity.new
-  # end
+
+  def test_it_has_participants
+    assert_equal ([]), @activity.participants
+  end
+
+  def test_can_add_participants
+    participant = {name: "Fred", amount: 20}
+    @activity.add_particpants(participant)
+    
+    assert_equal ([{name: "Fred", amount: 20}]), @activity.participants
+  end
+
+  def test_it_has_a_base_cost
+    assert_equal 0, @activity.base_cost(0)
+  end
+
+  def test_cost_per_person
+    participant = {name: "Fred", amount: 20}
+    @activity.add_particpants(participant)
+    participant = {name: "Alice", amount: 20}
+    @activity.add_particpants(participant)
+    @activity.base_cost(40)
+
+    assert_equal 20, @activity.cost_per_person
+  end
 end
